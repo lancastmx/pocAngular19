bf98f3588cf0b372f12572e7d972de3463d8f290
lancast <angelprep92@gmail.com>
2025-04-19 10:53:30 -0600

    feat:  rutas update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { LabComponent } from './pages/lab/lab.component';
 import { TodoComponent } from './pages/todo/todo.component';
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: LabComponent
+  // },
   {
     path: '',
-    component: LabComponent
-  },
-  {
-    path: 'todo',
     component:TodoComponent
   }
 ];
